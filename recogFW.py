744678b30e40e90898c0fe5091a517829a02906d
root <l.kyjanek@gmail.com>
2018-07-17 21:51:58 +0200

    universal rules (P. Pognan, 1983) almost done

--- recogFW.py
+++ recogFW.py
@@ -4,13 +4,85 @@
 '''Script for the recognition of foreign words in the Czech language.'''
 
 import sys
+import itertools as it
 
 
 # function for the recognition of foreign word
 # can be imported and used in any project
 def recog_foreign_word(word, pos=None):
     '''Return True statement if given word (and pos optionaly) is foreign.'''
-    return word, pos
+    word = word.lower()
+
+    # PART1: universal rules for all pos
+    # foreign letters
+    for letter in 'gxwqóf':
+        if letter in word:
+            return word, True
+
+    # combinations of vowels (except 'ou' and 'oo')
+    vowels = 'aeiyouáéěíýůú'
+
+    combinations = list(it.product(vowels, repeat=2))
+    del combinations[combinations.index(('o', 'u'))]
+    del combinations[combinations.index(('o', 'o'))]
+
+    for pair in combinations:
+        comb = pair[0] + pair[1]
+        if comb in word:
+            return word, True
+
+    # unrespected palatalization
+    palatal_vowels = 'eéií'
+    for pair in list(it.product(('k', 'h', 'ch', 'r'), palatal_vowels)):
+        comb = pair[0] + pair[1]
+        if comb in word:
+            return word, True
+
+    # unrespected soft and hard consonants
+    hard_consonants = ('h', 'ch', 'k', 'r')
+    for pair in list(it.product(hard_consonants, 'ií')):
+        comb = pair[0] + pair[1]
+        if comb in word:
+            return word, True
+
+    soft_consonants = 'žščřcj'
+    for pair in list(it.product(soft_consonants, 'yý')):
+        comb = pair[0] + pair[1]
+        if comb in word:
+            return word, True
+
+    # unrespected mixed consonants
+
+    # borrowed nasalisation pattern
+    consonants = 'cdghjklrstvzščřžďťň'
+    vowels = 'aeiyouáéěíýůú'
+
+    transcribed = ''
+    for letter in word:
+        if letter in consonants:
+            transcribed += 'C'
+        elif letter in vowels:
+            transcribed += 'V'
+        else:
+            transcribed += letter
+
+    if 'VnC' in transcribed or 'Vmb' in transcribed or 'Vmp' in transcribed:
+        return word, True
+
+    # PART2: rules according to a specific pos
+    if pos == 'N' or pos is None:  # nouns and unspecific
+        pass
+
+    if pos == 'V' or pos is None:  # verbs and unspecific
+        pass
+
+    if pos == 'A' or pos is None:  # adjectives and unspecific
+        pass
+
+    if pos == 'D' or pos is None:  # adverbs and unspecific
+        pass
+
+    return word, False
 
 
 # internal function for splitting input data if script is used in shell
@@ -34,7 +106,7 @@ def split_data(line):
 # running script if it is used in shell (with stdin or path to file)
 if __name__ == '__main__':
 
-    if not sys.stdin.isatty():  # read from stdin (pipeline)
+    if not sys.stdin.isatty():  # read from stdin
         for line in sys.stdin:
             word, pos = split_data(line.strip())
             print(recog_foreign_word(word, pos))
