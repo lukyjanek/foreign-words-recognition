2b0b341929c85ec1b68645e3f565195cd63e02cd
root <l.kyjanek@gmail.com>
2018-07-18 11:23:04 +0200

    added rule based on mixed consonant patterns (inspired by P. Pognan, 1883)

--- recogFW.py
+++ recogFW.py
@@ -17,7 +17,7 @@ def recog_foreign_word(word, pos=None):
     # foreign letters
     for letter in 'gxwqóf':
         if letter in word:
-            return word, True
+            return word, 1, True
 
     # combinations of vowels (except 'ou' and 'oo')
     vowels = 'aeiyouáéěíýůú'
@@ -29,29 +29,70 @@ def recog_foreign_word(word, pos=None):
     for pair in combinations:
         comb = pair[0] + pair[1]
         if comb in word:
-            return word, True
+            return word, 2, True
 
     # unrespected palatalization
     palatal_vowels = 'eéií'
     for pair in list(it.product(('k', 'h', 'ch', 'r'), palatal_vowels)):
         comb = pair[0] + pair[1]
         if comb in word:
-            return word, True
+            return word, 3, True
 
     # unrespected soft and hard consonants
     hard_consonants = ('h', 'ch', 'k', 'r')
     for pair in list(it.product(hard_consonants, 'ií')):
         comb = pair[0] + pair[1]
         if comb in word:
-            return word, True
+            return word, 4, True
 
     soft_consonants = 'žščřcj'
     for pair in list(it.product(soft_consonants, 'yý')):
         comb = pair[0] + pair[1]
         if comb in word:
-            return word, True
+            return word, 5, True
 
-    # unrespected mixed consonants
+    # unrespected y followed by mixed consonants (b, l, m, p, s, v, z)
+    vowels_long = 'áéíýůú'
+    vowels_all = 'aeiyouáéíýůú'
+
+    by = ['byt', 'byl', 'aby', 'kdyby', 'čby', 'bych', 'bys@', 'byste', 'byv',
+          'bydl', 'byč', 'byst', 'bysl']
+    ly = ['slyš', 'mlyn', 'lyka', 'lyká', 'plyn', 'vzlyk', 'lysý', 'lysi',
+          'lyso', 'lysic', 'lyž', 'lyň', 'plyš']
+    my = ['myd', 'myt', 'myč', 'myc', 'myj', 'mysl', 'myšl', 'myl', 'hmyz',
+          'myš', 'myk', 'mych']
+    py = ['pych', 'pyt', 'pyl', 'pysk', 'pyka', 'pyká']
+    sy = ['syn@', 'synVA', 'sysl', 'syse', 'sytVA', 'syt@', 'syre',
+          'syro', 'sychr', 'sycha', 'syč', 'syk', 'syp']
+    vy = ['vys', 'vyš', 'vyk', 'vyd', 'vyž', '#vy']
+    zy = ['brzy', 'jazyk', 'jazyl', 'zýv']
+
+    all_patterns = {'by': by, 'ly': ly, 'my': my, 'py': py,
+                    'sy': sy, 'vy': vy, 'zy': zy}
+
+    for name, patterns in all_patterns.items():
+        checked = word
+        if checked.endswith('y'):
+            checked = checked[:-1]
+
+        for pattern in patterns:
+            if 'VD' in pattern:  # long vowel insted 'VD'
+                for letter in vowels_long:
+                    checked = checked.replace(pattern[:-2] + letter, '$')
+            elif 'VA' in pattern:  # vowel instead 'VA'
+                for letter in vowels_all:
+                    checked = checked.replace(pattern[:-2] + letter, '$')
+            elif '@' in pattern:  # word ends with this pattern
+                if checked.endswith(pattern[:-1]):
+                    checked = checked[:-len(pattern)-1]
+            elif '#' in pattern:  # word starts with this pattern
+                if checked.startswith(pattern[1:]):
+                    checked = checked[len(pattern[1:]):]
+            else:
+                checked = checked.replace(pattern, '$')
+
+        if name in checked:
+            return word, 6, True
 
     # borrowed nasalisation pattern
     consonants = 'cdghjklrstvzščřžďťň'
@@ -67,7 +108,7 @@ def recog_foreign_word(word, pos=None):
             transcribed += letter
 
     if 'VnC' in transcribed or 'Vmb' in transcribed or 'Vmp' in transcribed:
-        return word, True
+        return word, 7, True
 
     # PART2: rules according to a specific pos
     if pos == 'N' or pos is None:  # nouns and unspecific
